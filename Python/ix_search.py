9cb71398ac43d7746fce704f3e64275ca6ab09c1
etherverser <46454041+etherverser@users.noreply.github.com>
2019-02-08 15:08:28 -0500

    Update ix_search.py

--- Python/ix_search.py
+++ Python/ix_search.py
@@ -7,12 +7,16 @@ from termcolor import colored
 import urllib, urllib2
 
 
-def ix_search(baseurl,apikey,term):
+def ix_search(baseurl,term):
+
+    print baseurl
+    print apikey
+    print term
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5",
-        "x-key": apikey,
-        "Host": urlbase,
+        "x-key": '9df61df0-84f7-4dc7-b34c-8ccfb8646ace',
+        "Host": baseurl,
         "Content-Length": "212"
         }
 
@@ -29,42 +33,49 @@ def ix_search(baseurl,apikey,term):
         "terminate": []
     } 
 
-    searchurl = url + "/intelligent/search"
-    getid = requests.post(str(searchurl),data=json.dumps(payload),headers=headers)
-    id_response = getid.json()
+    searchurl = 'https://' + baseurl + '/intelligent/search'
+    print searchurl
+    getid = requests.post("https://public.intelx.io/intelligent/search",data=json.dumps(payload),headers=headers)
 
-    #Authenticate to API
-    if id_response['status'] == 0:
-        print colored("[+]Successful API Authentication. Starting records search.","green")
-        #Craft API URL with the id to return results
-        resulturl = str(searchurl) + "/result?id=%s" %str(id_response['id'])
+    if getid.status_code == 200:
+        id_response = getid.json()
 
-        getresults = requests.get(resulturl,headers=headers)
-        #print getresults
-        data = getresults.json()
-        
-        if data['status'] == 0 or data['status'] == 1:
-            #print data in json format to manipulate as desired
-            print data
-        else:
-            print "----------------------------------------------"
-            print "[!] Error Code Status: <" + str(data['status']) + ">"
-            print "----------------------------------------------"
-            print "Code <2> | Search ID Not Found."
-            print "Code <3> | No Results at this time. Try later."
-            print "----------------------------------------------"
+        #Authenticate to API
+        if id_response['status'] == 0:
+            print colored("[+]Successful API Authentication. Starting records search.","green")
+            #Craft API URL with the id to return results
+            resulturl = str(searchurl) + "/result?id=%s" %str(id_response['id'])
 
-    if id_response['status'] == 1:
-        print "[!]Invalid term used."
+            getresults = requests.get(resulturl,headers=headers)
+            #print getresults
+            data = getresults.json()
+            
+            if data['status'] == 0 or data['status'] == 1:
+                #print data in json format to manipulate as desired
+                print data
+            else:
+                print "----------------------------------------------"
+                print "[!] Error Code Status: <" + str(data['status']) + ">"
+                print "----------------------------------------------"
+                print "Code <2> | Search ID Not Found."
+                print "Code <3> | No Results at this time. Try later."
+                print "----------------------------------------------"
 
-    if id_response['status'] == 2:
-        print "[!] Reached the MAX number of concurrent connection for this API Key."
-    #print id_response['id']
-
+    else:
+        print "----------------------------------------------"
+        print "[!] Error Code Status: <" + str(getid.status_code) + ">"
+        print "----------------------------------------------"
+        print "204 | No Content"
+        print "400 | Bad Request. Invalid input."
+        print "401 | Unauthorized. Access not authorized."
+        print "402 | Payment Required. No credits available"
+        print "404 | Not Found. Item or identifier not found."
+        print "----------------------------------------------"
 
 if __name__ == "__main__":
-    #https://public.intelx.io/intelligent/search
+    #https://public.intelx.io
     #9df61df0-84f7-4dc7-b34c-8ccfb8646ace
     #python ix_search.py <baseurl> <apikey> <selector>
-    ixsearch(sys.argv[1],sys.argv[2],sys.argv[3])
+    #python ix_search.py https://public.intelx.io 9df61df0-84f7-4dc7-b34c-8ccfb8646ace test.com
+    ix_search(sys.argv[1],sys.argv[2],sys.argv[3])
 
